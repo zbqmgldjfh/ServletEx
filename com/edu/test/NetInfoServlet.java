df07c528f3403efc712798bce64c5906e425f8f4
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-08-24 10:28:22 +0900

    NetInfoServlet 구현
    서버와 클라이언트 정보 확인

---
+++ com/edu/test/NetInfoServlet.java
@@ -0,0 +1,27 @@
+package com.edu.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/netInfo")
+public class NetInfoServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("test/html;charset=EUC-KR");
+		PrintWriter out = res.getWriter();
+		out.print("<html>");
+		out.print("<head><title>Request 정보 출력 Servlet</title></head>");
+		out.print("<body>");
+		out.print("<h3>네트워크 관련 요청 정보</h3>");
+		out.print("Request Scheme : " + req.getScheme() + "</br>");
+		out.print("Server Name : " + req.getServerName() + "</br>");
+		out.print("Server Address : " + req.getLocalAddr() + "</br>");
+		out.print("Server Port : " + req.getServerPort() + "</br>");
+		out.print("Client Address : " + req.getRemoteAddr() + "</br>");
+		out.print("Client Host : " + req.getRemoteHost() + "</br>");
+		out.print("Client Port : " + req.getRemotePort() + "</br>");
+		out.print("</body></html>");
+		out.close();
+	}
+}
