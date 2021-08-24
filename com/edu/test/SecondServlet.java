3b25f2e20010edb5034f892a1e7e9c2e03c32f0d
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-08-24 09:24:59 +0900

    Initial commit

---
+++ com/edu/test/SecondServlet.java
@@ -0,0 +1,19 @@
+package com.edu.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("SecondServlet!!");
+		PrintWriter out = resp.getWriter();
+		out.print("<html><head><title>Test</title></head>");
+		out.print("<body><h1>hava a nice day!!</h1></body>");
+		out.print("</html>");
+		out.close();
+	}
+}
